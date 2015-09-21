ffec20b39396de28361b13060b103b92712e62af
foxhound24 <foxhound24@live.com>
2015-09-21 13:23:25 -0400

    -Added list_item_forecast.xml layout -Changed fragment_main.xml to use listview over textview -added an ArrayList of temporary forecast data in the MainActivityFragment.java file

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package com.example.android.sunshine.app;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,16 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ArrayList<String> tempData = new ArrayList<String>();
+        tempData.add("Today - Sunny - 88/63");
+        tempData.add("Tomorrow - Foggy - 70/46");
+        tempData.add("Weds - Cloudy - 72/63");
+        tempData.add("Thurs - Rainy - 64/51");
+        tempData.add("Fri - Foggy - 70/46");
+        tempData.add("Sat - Sunny - 76/68");
+
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
